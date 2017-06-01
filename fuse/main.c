be51ae5e3f0b1b0cb5146dc952862977468704a0
Pablo Mendez Hernandez <pablomh@gmail.com>
2017-06-01 10:34:25 +0000

    libfuse3: ifdef big_writes mount option
    Starting from version 3.0.0 it's always active

--- fuse/main.c
+++ fuse/main.c
@@ -41,7 +41,10 @@
 	#error FUSE 2.6 or later is required
 #endif
 
-const char* default_options = "ro_fallback,allow_other,blkdev,big_writes,"
+const char* default_options = "ro_fallback,allow_other,blkdev,"
+#if FUSE_USE_VERSION < 30
+		"big_writes,"
+#endif
 		"default_permissions";
 
 struct exfat ef;
